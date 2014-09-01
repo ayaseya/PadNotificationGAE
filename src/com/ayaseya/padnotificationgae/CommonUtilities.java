b918970c932726ff1bf71bac63d46b217d6e9d2f
ayaseya <ayaseya@ayaseya-PC>
2014-09-01 20:23:52 +0900

    サーバー用APIがプロジェクトayaseya-dev-jpのものになっていたため正しいものに修正

--- src/com/ayaseya/padnotificationgae/CommonUtilities.java
+++ src/com/ayaseya/padnotificationgae/CommonUtilities.java
@@ -20,7 +20,7 @@ public final class CommonUtilities {
      * https://console.developers.google.com/
      * サーバー用APIキー＝SERVER_API_KEY
      */
-    static final String SERVER_API_KEY = "AIzaSyDGVth9Itouflh8_47bYX8hsEJlXQCNS2U";
+    static final String SERVER_API_KEY = "AIzaSyBufE0jbqYT7Q5J891r1bUiF9oq-qIWAXc";
 
 
 }
